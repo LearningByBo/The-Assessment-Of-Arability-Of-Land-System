b73e5dab400483daba8792d566709303ac36e77b
Bo <872882406@qq.com>
2017-02-20 18:36:49 +0800

    更新了svm模型

--- code/System/models.py
+++ code/System/models.py
@@ -101,11 +101,11 @@ class Factor(models.Model):
             answer = knn_clf.predict(x)
             score = 0.51
         elif al_select == 'svm':
-            # score : 0.75 0.70 0.69 20
+            # score : 0.91 0.85 0.85 20
             fname = "svmtree.dot"
             svm_clf = joblib.load(sys.path[0] + '\\static\\model_file\\' + fname)
             answer = svm_clf.predict(x)
-            score = 0.69
+            score = 0.85
         elif al_select == 'lr':
             # score : 0.69 0.70 0.69 20
             fname = "lrtree.dot"
@@ -152,11 +152,12 @@ class Factor(models.Model):
         # joblib.dump(knn_clf, sys.path[0] + '\\static\\model_file\\' + fname,compress=3)
 
         # SVM Fit
-        svm_clf_rbf = svm.SVC()
+        # C represent the request of the precision , but too large may cause overfitting
+        svm_clf_linear = svm.LinearSVC(C=3.5)
         # print(svm_clf_rbf)
-        svm_clf_rbf.fit(x_train, y_train)
+        svm_clf_linear.fit(x_train, y_train)
         # fname = "svmtree.dot"
-        # joblib.dump(svm_clf_rbf, sys.path[0] + '\\static\\model_file\\' + fname,compress=3)
+        # joblib.dump(svm_clf_linear, sys.path[0] + '\\static\\model_file\\' + fname,compress=3)
 
         # LR Fit
         lr_clf = linear_model.LogisticRegression()
@@ -194,15 +195,15 @@ class Factor(models.Model):
         knn_report = classification_report(y, knn_x_answer,target_names=['class 1', 'class 2', 'class 3', 'class 4', 'class 5','class 6'])
 
         # 测试结果的打印
-        svm_answer = svm_clf_rbf.predict(x_train)
+        svm_answer = svm_clf_linear.predict(x_train)
         # print(x_train)
         # print(svm_answer)
         # print(y_train)
         # print(np.mean(answer == y_train))
 
         # 准确率与召回率
-        precision, recall, fbeta_score, support = precision_recall_fscore_support(y_test, svm_clf_rbf.predict(x_test))
-        svm_x_answer = svm_clf_rbf.predict(x)
+        precision, recall, fbeta_score, support = precision_recall_fscore_support(y_test, svm_clf_linear.predict(x_test))
+        svm_x_answer = svm_clf_linear.predict(x)
         # print svm_x_answer
         # print y
         # print(classification_report(y, svm_x_answer,target_names=['class 1', 'class 2', 'class 3', 'class 4', 'class 5', 'class 6']))
@@ -234,7 +235,7 @@ class Factor(models.Model):
         clf = []
         clf.append(dt_clf)
         clf.append(knn_clf)
-        clf.append(svm_clf_rbf)
+        clf.append(svm_clf_linear)
         clf.append(lr_clf)
 
         clf_data = []
